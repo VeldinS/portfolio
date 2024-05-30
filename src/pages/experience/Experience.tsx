11913d94e210d3d294f9d992213a1ee47dfbd302
VeldinS <veldinsalcinovic123@gmail.com>
2024-05-30 22:26:08 +0200

    CV button updated

--- src/pages/experience/Experience.tsx
+++ src/pages/experience/Experience.tsx
@@ -30,7 +30,7 @@ function Experience() {
                         <span className='page-info-heading-span'> Full-stack Software Engineer. </span>
                         {"/>"}
                     </h1>
-                    <Link target="_blank" rel="noopener noreferrer" to={"https://drive.google.com/file/d/1OlU5W8xeHP5l7vkOrF7WceqN-C5NcrdO/view?usp=sharing"}>
+                    <Link target="_blank" rel="noopener noreferrer" to={"https://drive.google.com/file/d/1p2Pby0NyxchP92MQufJUtQ9qg0lC1JaF/view?usp=sharing"}>
                         <button className={"cv-button"}>
                             Download CV
                         </button>
