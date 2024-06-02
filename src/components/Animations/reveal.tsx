415fcbdbc27656a028e077fc9896ea96bfcc96be
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-02 22:59:45 +0200

    Animations implemented

---
+++ src/components/Animations/reveal.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from 'react';
+
+// @ts-ignore
+const RevealComp = ({ children, threshold, duration, delay, x, y }) => {
+    x = x || 0;
+    y = y || 0;
+    const ref = useRef(null);
+    const [intersecting, setIntersecting] = useState(false);
+
+    useEffect(() => {
+        if (ref.current) {
+            const intersectionObserver = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting) {
+                    setIntersecting(true);
+                } else {
+                    setIntersecting(false);
+                }
+            }, {
+                threshold
+            });
+
+            intersectionObserver.observe(ref.current);
+
+            return () => {
+                if (ref.current) {
+                    intersectionObserver.unobserve(ref.current);
+                }
+            };
+        }
+    }, [threshold]);
+
+    return (
+        <div
+            style={{
+                transitionDuration: duration,
+                transitionDelay: delay,
+                transform: !intersecting ? `translate(${x}px, ${y}px)` : "translate(0px, 0px)"
+            }}
+            className={`transition ${intersecting ? "opacity-100" : "opacity-0"}`}
+            ref={ref}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default RevealComp;
--- src/components/ExperienceCard/ExpCard.tsx
+++ src/components/ExperienceCard/ExpCard.tsx
@@ -1,5 +1,6 @@
 import './expCard.css'
 import '../EducationCard/eduCard.css'
+import RevealComp from '../Animations/reveal';
 
 
 interface expCard{
@@ -15,28 +16,30 @@ interface expCard{
 
 function ExpCard(props:expCard) {
     return (
-        <div className={"experience-card"}>
-            <div className={"education-info experience-info"}>
-                <h1 className={"education-heading"}>{props.cardName}</h1>
-                <p className={"education-field"}>{props.cardEmploymentType}</p>
-                <div className={"education-date-wrapper"}>
-                    <p className={"education-date"}>{props.startDate}</p>
-                    {props.endDate && (
-                        <div className={"circle"}></div>
-                    )}
-                    {props.endDate && (
-                        <p className={"education-date"}>{props.endDate}</p>
-                    )}
+        <RevealComp delay={'200ms'} threshold={0.5} duration={'300ms'} x={-30} y={0}>
+            <div className={"experience-card"}>
+                <div className={"education-info experience-info"}>
+                    <h1 className={"education-heading"}>{props.cardName}</h1>
+                    <p className={"education-field"}>{props.cardEmploymentType}</p>
+                    <div className={"education-date-wrapper"}>
+                        <p className={"education-date"}>{props.startDate}</p>
+                        {props.endDate && (
+                            <div className={"circle"}></div>
+                        )}
+                        {props.endDate && (
+                            <p className={"education-date"}>{props.endDate}</p>
+                        )}
+                    </div>
+                    <p className={"education-date"}>{props.location}</p>
                 </div>
-                <p className={"education-date"}>{props.location}</p>
-            </div>
 
-            <div className={"experience-details"}>
-                <p className={"company-name"}>{props.company}</p>
-                <p className={"experience-paragraph"}>{props.paragraph1}</p>
-                <p className={"experience-paragraph"}>{props.paragraph2}</p>
+                <div className={"experience-details"}>
+                    <p className={"company-name"}>{props.company}</p>
+                    <p className={"experience-paragraph"}>{props.paragraph1}</p>
+                    <p className={"experience-paragraph"}>{props.paragraph2}</p>
+                </div>
             </div>
-        </div>
+        </RevealComp>
     );
 }
 
