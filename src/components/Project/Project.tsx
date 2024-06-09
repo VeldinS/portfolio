0f2d9998e8a5ba2709503bac0d66f7370181b8a9
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-09 17:27:08 +0200

    Projects page - animations added

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,5 +1,6 @@
 import './project.css';
 import {Link} from "react-router-dom";
+import RevealComp from "../Animations/reveal.tsx";
 
 interface ProjectProps {
     alignment: string,
@@ -14,18 +15,26 @@ function Project(ProjectProps: ProjectProps): JSX.Element {
     return (
         <div className={`project-wrapper-${ProjectProps.alignment}`}>
             <div className={`project-heading-wrapper-${ProjectProps.alignment}`}>
-                <div className={`project-heading-${ProjectProps.alignment}`}>
-                    <h1 className={'project-main-heading'}>{ProjectProps.title}</h1>
-                    <h3 className={'project-main-subheading'}>{ProjectProps.subtitle}</h3>
-                </div>
-                <p className={`project-text-${ProjectProps.alignment}`}>{ProjectProps.description}</p>
-                <Link target="_blank" rel="noopener noreferrer" to={ProjectProps.link}>
-                    <button className={"project-button"}>
-                        Check it out...
-                    </button>
-                </Link>
+                <RevealComp delay={'200ms'} threshold={0.2} duration={'300ms'} x={-30} y={0}>
+                    <div className={`project-heading-${ProjectProps.alignment}`}>
+                        <h1 className={'project-main-heading'}>{ProjectProps.title}</h1>
+                        <h3 className={'project-main-subheading'}>{ProjectProps.subtitle}</h3>
+                    </div>
+                </RevealComp>
+                <RevealComp delay={'600ms'} threshold={0.2} duration={'300ms'} x={-30} y={0}>
+                    <p className={`project-text-${ProjectProps.alignment}`}>{ProjectProps.description}</p>
+                </RevealComp>
+                    <Link target="_blank" rel="noopener noreferrer" to={ProjectProps.link}>
+                        <RevealComp delay={'800ms'} threshold={0.2} duration={'300ms'} x={-30} y={0}>
+                            <button className={"project-button"}>
+                                Check it out...
+                            </button>
+                        </RevealComp>
+
+                    </Link>
                 <div className={`linear-gradient-project-${ProjectProps.alignment}`}></div>
             </div>
+
             <img className={'project-image'} src={ProjectProps.image} alt={'project image'} />
         </div>
     );
